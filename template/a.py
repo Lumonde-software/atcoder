9a4b74e2aa611872e1d1df0d8d6a8511c0552adf
Lumonde-software <nakamura-shoya-1@g.ecc.u-tokyo.ac.jp>
2024-06-02 19:00:54 +0900

    update templates

--- template/a.py
+++ template/a.py
@@ -1,12 +1,12 @@
-import sys
 import random
-from math import gcd, lcm, sqrt, isqrt, perm, comb, factorial, log2, ceil, floor
-from collections import Counter, defaultdict, deque
-from functools import lru_cache, reduce, cmp_to_key
-from itertools import accumulate, combinations, permutations
-from heapq import nsmallest, nlargest, heappushpop, heapify, heappop, heappush
-from copy import deepcopy
+import sys
 from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import accumulate, combinations, permutations
+from math import ceil, comb, factorial, floor, gcd, isqrt, lcm, log2, perm, sqrt
 from string import ascii_lowercase, ascii_uppercase
 
 inf = float("inf")
@@ -22,6 +22,4 @@ SEI = lambda m: [list(input().split()) for _ in range(m)]
 NEI = lambda m: [list(map(int, input().split())) for _ in range(m)]
 dir4 = ((-1, 0), (0, 1), (1, 0), (0, -1))
 dir8 = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
-mod99 = 998244353
-mod = 10**9 + 7
-RANDOM = random.randint(1217, 2000)
+mod = 998244353
